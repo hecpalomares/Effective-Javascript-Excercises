8d18c174f6d46929238b34c0c3188f9d344d1de6
hecpalomares <hpalomares12@gmail.com>
2017-10-19 23:42:13 -0500

    README.md Module #1

---
+++ 1. Accustoming Yourself to Javascript/item-1.js
@@ -0,0 +1,9 @@
+//	Wrap your code the contents of the code in funtions to enable strict mode locally
+(function() {
+	"use strict";
+	
+	function f() {
+		let arguments = [];
+	}
+
+}());
---
+++ 1. Accustoming Yourself to Javascript/item-2.js
@@ -0,0 +1,6 @@
+//	Javascript numbers are double-precision
+//	Limitations of floating point arithmethic
+(function() {
+	// Floating point aithmethic produce approximate results
+	console.log(.1 + .2);
+}())
