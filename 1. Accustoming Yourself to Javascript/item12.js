ea665a2c21bde3b6c75b4d2c52ae161aa77d1586
hecpalomares <hpalomares12@gmail.com>
2017-10-26 20:30:35 -0500

    Item 12: Hoisting variables

---
+++ 1. Accustoming Yourself to Javascript/item12.js
@@ -0,0 +1,52 @@
+/**	Variable hosting **/
+//	Variables declarations are implicitly hoisted to the top of their enclosing functions
+//	Redeclarations are treated as a single variable
+//	Manually hoisting local variable declarations to avoid confusion
+	Javascript is lexical scoped
+function variablesNotHoisted(header, body, footer) {
+	for(var i = 0, n = header.length; i < n; i++) {
+		header[i] = header[i].trim();
+	}
+
+	for(var i = 0, n = body.length; i < n; i++) {
+		body[i] = body[i].trim();
+	}
+
+	for(var i = 0, n = footer.length; i < n; i++) {
+		footer[i] = footer[i].trim();
+	}
+}
+
+function manuallyVariablesHoisted(header, body, footer) {
+	var i, n;
+	for(i = 0; n = header.length; i < n; i++) {
+		header[i] = header[i].trim();
+	}
+
+	for(i = 0; n = body.length; i < n; i++) {
+		body[i] = body[i].trim();
+	}
+
+	for(i = 0; n = footer.length; i < n; i++) {
+		footer[i] = footer[i].trim();
+	}
+}
+
+// Block scoped try() and catch()
+function blockedScoped() {
+	var x = "scoped";
+	var result = [];
+	result.push(x);
+
+	try {
+		throw "exception";
+	} catch(x) {
+		x = "catch";
+	}
+
+	result.push(x);
+	return result;
+
+}
+
+console.log(blockedScoped());
