47ffcefaa3bb26dd35c1499ddf85c2577b78324e
hecpalomares <hpalomares12@gmail.com>
2017-11-05 18:07:15 -0600

    Item 25: Bind method

---
+++ 3. Working With Functions/item25.js
@@ -0,0 +1,28 @@
+//	Bind to extract methods with a fixed receiver
+let buffer = {
+	entries: [],
+	add: function(string) {
+		this.entries.push(string);
+	},
+	concat: function() {
+		return this.entries.join("");
+	},
+	empty: function() {
+		this.entries.length = 0;
+	}
+};
+
+let source = ["555", "-", "1234", "-", "5678"];
+source.forEach(buffer.add, buffer);
+
+buffer.concat();
+
+console.log(buffer.concat());
+
+buffer.empty();
+
+//	Function objets with bind method that takes receiver object
+//	produces wrapper function that calls the original function as a method of the receiver
+let sourceBind = ["333", "-", "9876", "-", "5432"];
+sourceBind.forEach(buffer.add.bind(buffer));
+console.log(buffer.concat());
