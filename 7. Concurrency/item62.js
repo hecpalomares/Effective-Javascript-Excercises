48a7d6ab9ac6087714c3ae4f613175f079883fad
hecpalomares <hpalomares12@gmail.com>
2017-12-02 18:50:23 -0600

    Item62: Nested and Named Callbacks. Added comments and downloadAllSync example

--- 7. Concurrency/item62.js
+++ 7. Concurrency/item62.js
@@ -1,7 +1,7 @@
 //	Nested or Named Callbacks for Async Sequencing
-//	* Use nested or named callbacks to perforn several asynchornous operations in sequence
-//	* Balance out named callbacks and nested to make readable code
-//	* Avoid sequencing operations that should be done concurrently
+//	* Use nested or named callbacks to perforn several asynchornous operations in sequence (lines 9-11)
+//	* Balance out named callbacks and nested to make readable code (lines 9-27)
+//	* Avoid sequencing operations that could be done concurrently (line 29-34)
 
 let lookupURL = "https://example.com"
 
@@ -20,9 +20,15 @@ function downloadFiles(url, file) {
 	downloadSync("a.txt", function(a) {
 		downloadSync("b.txt", function(b) {
 			downloadSync("c.txt", function(c) {
-				//
+				//	Some code
 			});
 		});
 	});
 }
 
+function downloadAllFiles(url, file) {
+	downloadAllSync(["a.txt", "b.txt", "c.txt"], function(all) {
+		let a = all[0], b = all[1], c = all[2];
+		//	Work with variables a, b and c
+	})
+}
